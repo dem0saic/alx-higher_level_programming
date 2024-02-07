ea96fd0f35e0c4eec268a460caa68df4dbfa7fe7
Owusu Vincent <144287125+dem0saic@users.noreply.github.com>
2024-02-07 01:41:36 +0000

    Write a class Student that defines a student by: (based on 9-student.py).

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained in this
+        list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
